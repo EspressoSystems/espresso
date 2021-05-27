21108b05bb14859c8b5ecddc8ac0e205a5fe4969
John D. Corbett <corbett@pictographer.com>
2021-05-27 15:47:19 -0700

    Converted println to proper logging

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,54 @@
+// Copyright © 2021 Translucence Research, Inc. All rights reserved.
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread;
-use std::time::SystemTime;
+use structopt::StructOpt;
+use tracing::info;
 
 const VALIDATOR_COUNT: u8 = 4;
 
 type Transaction = u32;
 
+#[derive(StructOpt)]
+/// Translucence Ledger Demo 1
+///
+/// Submits a number of transactions to consensus validation and
+/// records successful transactions to the ledger
+pub struct Args {
+    #[structopt(verbatim_doc_comment, long, short = "c", default_value = "1")]
+    pub transaction_count: u8,
+}
+
 fn load_crs() {
-    println!("Loading common reference string");
+    info!("Loading common reference string");
 }
 
 fn build_transaction(specification: Transaction) -> Transaction {
-    println!("Building transaction");
+    info!("Building transaction");
     specification
 }
 
 fn submit_transaction(tx: Sender<bool>, transaction: Transaction) {
-    println!("Submitting transaction to {} validators", VALIDATOR_COUNT);
+    info!("Submitting transaction to {} validators", VALIDATOR_COUNT);
     for id in 0..VALIDATOR_COUNT {
         propose_transaction(id, tx.clone(), transaction);
     }
 }
 
 fn propose_transaction(id: u8, tx: Sender<bool>, transaction: Transaction) {
-    println!(
+    info!(
         "Proposing transaction value: {} to validator {}",
         transaction, &id
     );
     thread::spawn(move || {
-        println!("  Validator {} received {}", id, &transaction);
+        info!("  Validator {} received {}", id, &transaction);
         let validity = validate_transaction(transaction);
-        tx.send(validity).unwrap();
+        tx.send(validity).expect("send failed");
     });
 }
 
 fn validate_transaction(_transaction: Transaction) -> bool {
+    use std::time::SystemTime;
+
     let now = SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
         .expect("Time is such a pain")
@@ -44,13 +58,12 @@ fn validate_transaction(_transaction: Transaction) -> bool {
 }
 
 fn consense(rx: Receiver<bool>) -> bool {
-    println!("Consensing");
+    info!("Consensing");
 
     let mut unanimous = true;
-
     for _ in 0..VALIDATOR_COUNT {
         let next = rx.recv().unwrap();
-        println!("  Validity is {}", &next);
+        info!("  Validity is {}", &next);
         if !&next {
             unanimous = false;
         }
@@ -60,22 +73,26 @@ fn consense(rx: Receiver<bool>) -> bool {
 }
 
 fn log_transaction(transaction: Transaction) {
-    println!("Recording transaction value: {}", transaction);
+    info!("Recording transaction value: {}", transaction);
 }
 
 fn main() {
+    tracing_subscriber::fmt::init();
+    let args = Args::from_args();
+
     load_crs();
 
-    for i in 0..2 {
-        let transaction = build_transaction(i + 42);
+    for i in 0..args.transaction_count {
+        info!("⬤  TRANSACTION {}", i);
+        let transaction = build_transaction(i as Transaction + 42);
         let (tx, rx) = channel();
         submit_transaction(tx, transaction);
         if consense(rx) {
-            println!("Consensus was achieved");
+            info!("Consensus was achieved");
             log_transaction(transaction);
         } else {
-            println!("Consensus was not achieved");
-            println!("Rejecting transaction");
+            info!("Consensus was not achieved");
+            info!("Rejecting transaction");
         }
     }
 }
