c5da3025e46ddc2fcde46ad56152718c5db75319
John D. Corbett <corbett@pictographer.com>
2021-06-29 09:03:55 -0700

    Added web socket support via Tungstenite

--- examples/tideui/src/main.rs
+++ examples/tideui/src/main.rs
@@ -1,7 +1,13 @@
 // Copyright © 2021 Translucence Research, Inc. All rights reserved.
-use tide::Request;
+use async_std::task::block_on;
+use std::{net::TcpListener, thread::spawn};
+use tide;
+use tungstenite::{
+    accept_hdr,
+    handshake::server::{Request, Response},
+};
 
-async fn echo(req: Request<()>) -> tide::Result<String> {
+async fn report_params(req: tide::Request<()>) -> tide::Result<String> {
     tide::log::debug!("Request {:?}", &req);
     let res = format!(
         "Sent {} jellybeans to {}.\n",
@@ -14,11 +20,38 @@ async fn echo(req: Request<()>) -> tide::Result<String> {
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
     tide::log::start();
+
+    // Initialize and start the HTTP server
     let mut app = tide::new();
     app.at("/transfer").serve_file("src/transfer.html")?;
     app.at("/transfer/").serve_file("src/transfer.html")?;
-    app.at("/transfer/:raddr/:amt").get(echo);
+    app.at("/transfer/:raddr/:amt").get(report_params);
     app.at("*").get(|_| async { Ok("Whatever") });
-    app.listen("127.0.0.1:8080").await?;
+    spawn(move || block_on(app.listen("127.0.0.1:8080")));
+
+    // From tungstenite server.rs
+    // Initialize and start the Web Socket server
+    let server = TcpListener::bind("127.0.0.1:3012").unwrap();
+    for stream in server.incoming() {
+        spawn(move || {
+            let callback = |_req: &Request, response: Response| Ok(response);
+            let mut websocket = accept_hdr(stream.unwrap(), callback).unwrap();
+            websocket
+                .write_message("Web Socket server says hello.".into())
+                .unwrap();
+
+            let mut counter: usize = 0;
+            loop {
+                let msg = websocket.read_message().unwrap();
+                if msg.is_text() {
+                    websocket
+                        .write_message(format!("{}: {}", counter, msg).into())
+                        .unwrap();
+                    counter = counter + 1;
+                }
+            }
+        });
+    }
+
     Ok(())
 }
