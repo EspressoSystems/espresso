5935de2f85500488f4c286f37069f9eb5f54209a
John D. Corbett <corbett@pictographer.com>
2021-06-29 18:48:45 -0700

    Added tracing. Added state. Fixed (?) an unwrap error on read_message().

--- examples/tideui/src/main.rs
+++ examples/tideui/src/main.rs
@@ -1,27 +1,56 @@
 // Copyright © 2021 Translucence Research, Inc. All rights reserved.
 use async_std::task::block_on;
 use std::{net::TcpListener, thread::spawn};
+use tide;
+use tide_tracing::TraceMiddleware;
 use tungstenite::{
     accept_hdr,
     handshake::server::{Request, Response},
 };
 
-async fn report_params(req: tide::Request<()>) -> tide::Result<String> {
+#[derive(Clone, Debug)]
+struct State {
+    name: String,
+}
+
+async fn report_params(req: tide::Request<State>) -> tide::Result<String> {
     tide::log::debug!("Request {:?}", &req);
     let res = format!(
-        "Sent {} jellybeans to {}.\n",
+        "Sent {} jellybeans to {}. {}\n",
         req.param("amt")?,
-        req.param("raddr")?
+        req.param("raddr")?,
+        req.state().name
     );
     Ok(res)
 }
 
 #[async_std::main]
-async fn main() -> Result<(), std::io::Error> {
-    tide::log::start();
+async fn main() -> tide::Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
+
+    let state = State {
+        name: "nom nom".to_string(),
+    };
 
     // Initialize and start the HTTP server
-    let mut app = tide::new();
+    //    let mut app = tide::Server::new();
+    let mut app = tide::with_state(state);
+
+    app.with(TraceMiddleware::new());
+
+    app.at("/working_endpoint")
+        .get(|_| async { Ok(tide::Response::new(tide::StatusCode::Ok)) });
+    app.at("/client_error")
+        .get(|_| async { Ok(tide::Response::new(tide::StatusCode::NotFound)) });
+    app.at("/internal_error").get(|_| async {
+        tide::Result::<tide::Response>::Err(tide::Error::from_str(
+            tide::StatusCode::ServiceUnavailable,
+            "This message will be displayed",
+        ))
+    });
+
     app.at("/transfer").serve_file("src/transfer.html")?;
     app.at("/transfer/").serve_file("src/transfer.html")?;
     app.at("/transfer/:raddr/:amt").get(report_params);
@@ -41,12 +70,13 @@ async fn main() -> Result<(), std::io::Error> {
 
             let mut counter: usize = 0;
             loop {
-                let msg = websocket.read_message().unwrap();
-                if msg.is_text() {
-                    websocket
-                        .write_message(format!("{}: {}", counter, msg).into())
-                        .unwrap();
-                    counter += 1;
+                if let Ok(msg) = websocket.read_message() {
+                    if msg.is_text() {
+                        websocket
+                            .write_message(format!("{}: {}", counter, msg).into())
+                            .unwrap();
+                        counter += 1;
+                    }
                 }
             }
         });
