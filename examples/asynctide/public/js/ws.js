b883dd824068e8d04ccbc234ac46624b97f375d5
John D. Corbett <corbett@pictographer.com>
2021-07-27 10:09:45 -0700

    Extract fields from URL. Added documentation. Removed unused code. Improved some names.

--- examples/asynctide/public/js/ws.js
+++ examples/asynctide/public/js/ws.js
@@ -1,132 +1,127 @@
-let socket;
-let client_id;
+// Copyright © 2021 Translucence Research, Inc. All rights reserved.
+let webSocket;
+let clientId;
+// See https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent#status_codes
+const webSocketNormalClose = 1000;
 
-function isOpen() {
-    return socket && socket.readyState === socket.OPEN
+// Is the web socket connection open?
+function isWebSocketOpen() {
+    return webSocket && webSocket.readyState === webSocket.OPEN
 };
 
-function ioConn() {
-    let url = `${window.location.protocol === "https:" ? "wss" : "ws"}://${window.location.host}${window.location.pathname}`;
-    console.log(`ws.js:ioConn url:${url}`);
-    if (client_id) url += `?c=${client_id}`;
-    if (!isOpen()) socket = new WebSocket(url);
+// Attempt a web socket connection.
+function webSocketConnect() {
+    if (!isWebSocketOpen()) {
+        let protocol = window.location.protocol === "https:" ? "wss" : "ws";
+        let host = window.location.host;
+        let pathname = window.location.pathname;
+        let url = `${protocol}://${host}${pathname}`;
+        if (clientId) {
+            url += `?c=${clientId}`;
+        }
+        console.log(`ws.js:webSocketConnect url:${url}`);
+        webSocket = new WebSocket(url);
+        webSocket.onmessage = webSocketOnMessage;
+        status('Web Socket enabled.');
+    }
 }
 
-function uilog(msg) {
-    console.log(`ws.js:uilog:${msg}`);
+// Send a Web Sockets message to the server.
+async function webSocketSend(msg) {
+    if (!isWebSocketOpen()) {
+        webSocketConnect();
+        // TODO !corbett Why half a second? There must be a better condition.
+        await new Promise(r => setTimeout(r, 500)); // wait half second to connect
+    }
+    if (isWebSocketOpen()) {
+        status('webSocket: ' + msg);
+        webSocket.send(msg);
+    } else {
+        // TODO !corbett Happens when browsing to http://127.0.0.1:8080/
+        status('Unable to open WebSocket connection.');
+    }
+}
+
+// Append a message to the web page status text and console.
+function status(msg) {
+    console.log(`ws.js:status:${msg}`);
     let ta = document.getElementById('events');
     if (ta.value.length) { ta.value += '\n'; }
-    ta.value += msg;
+    ta.value += new Date().toUTCString() + ':' + msg;
     ta.scrollTop = ta.scrollHeight;
 }
 
-async function ioSend(msg) {
-    if (!isOpen()) {
-        ioConn();
-        // TODO Why half a second? There must be a better condition.
-        await new Promise(r => setTimeout(r, 500)); // wait half second to connect
-    }
-    if (isOpen()) {
-        uilog('ioSend ' + msg);
-        socket.send(msg);
-    } else {
-        // TODO !corbett Happens when browsing to http://127.0.0.1:8080/
-        uilog('Unable to open WebSocket connection.');
-    }
-}
-
-// TODO leftover
-function handleRestartGame() {
-    reset_by_me = true;
-    ioSend(`RESET:${client_id}`);
-}
-
+// Generate a clean URL from the form field values.
 function cleanUrl() {
     let raddr = document.getElementById("raddr").value;
     let amt = document.getElementById("amt").value;
-    return `/transfer/${raddr}/${amt}`;
+    return `/transfer/${clientId}/${raddr}/${amt}`;
 }
 
+// Redirect to a clean URL.
 function redirectClean() {
     console.log('redirectClean');
     window.location = cleanUrl();
     return false;
 }
 
-function onLoad() {
-    (function() {
-        socket.onmessage = function(msg) {
-            console.log(`ws.js:socket.onmessage:${msg.data}`);
-            const data = JSON.parse(msg.data);
-            uilog(`Got socket message: client_id:${data.client_id} msg:${data.msg}.`);
-        };
-        uilog('Web Socket enabled.');
-    })();
-}
-function onSend(){
-    uilog('Web Socket Send button pressed');
-    socket.send('Web Socket Send button pressed:' + cleanUrl());
+// React to a Web Sockets message from the server.
+function webSocketOnMessage(msg) {
+    const data = JSON.parse(msg.data);
+    status(`Got webSocket message: clientId:${data.clientId} msg:${data.msg}.`);
 }
 
+// Do a thing when the UI "Send" button is pressed.
+function onSend(){
+    status('Web Socket Send button pressed');
+    webSocket.send('Web Socket Send button pressed:' + cleanUrl());
+}
+
+// After the page is loaded (but possibly before all the assets have loaded),
+// open the Web Sockets connection and configure a message handler.
 document.addEventListener("DOMContentLoaded", function() {
     console.log('ws.js:Adding event listener for DOMContentLoaded');
 
-    ioConn();
+    webSocketConnect();
 
-    socket.addEventListener('message', message => {
+    webSocket.addEventListener('message', message => {
         const data = JSON.parse(message.data);
-        console.log(`ws.js: message event listener: client_id:${data.client_id} msg:${data.msg}`);
+        console.log(`ws.js: message event listener: clientId:${data.clientId} msg:${data.msg}`);
 
         switch (data.cmd) {
         case 'INIT':
-            console.log(`ws.js:Server sent INIT for client ${data.client_id}`);
-            client_id = data.client_id;
-            break;
-        case 'STATE':
-            gameState = data.play_book;
-            // redraw
-            redrawPlayBook();
-            if (!handleResultValidation() ) handlePlayerChange();
-            transfer_state = cleanUrl();
-
-            ioSend(`TRANSFER:${localPlayer}:${transfer_state}`);
-
-            break;
-        case 'RESET':
-            console.log('ws.js:reset');
-            if( ! reset_by_me ) alert( 'Other player just RESET the game' );
-
-            gameState = data.play_book;
-            // redraw
-            redrawPlayBook();
-            handlePlayerChange();
-            gameActive = localPlayer === currentPlayer ? true : false;
-            reset_by_me = false;
-            break;
-        case 'COMPLETE':
-            console.log('ws.js:COMPLETE');
-            alert( 'Oops... board complete!' );
-            statusDisplay.innerHTML = COMPLETE_MSG;
-            statusDisplay.classList.add("complete");
-            document.querySelector('.game--restart').classList.add("hidden");
-            gameActive = false;
+            console.log(`ws.js:Server sent INIT for client ${data.clientId}`);
+            clientId = data.clientId;
             break;
         case 'LEAVE':
             console.log('ws.js:leave');
-            alert('ws.js:Got LEAVE from other client');
+            alert('ws.js:Got LEAVE');
+            webSocket.close(1000, "Received a LEAVE command.");
+            webSocket = undefined;
             break;
         case 'TRANSFER':
-            uilog('TRANSFER ' + data.xfr);
+            // TODO !corbett Currently unused.
+            status('TRANSFER ' + data.xfr);
             break;
         default:
             console.log('ws.js: Received a message without a cmd field.');
         };
     });
+
+    console.log('Pathname: ' + window.location.pathname);
+    let re = /\/transfer\/([^/]+)\/([^/]+)\/(.*)/;
+    let ma = re.exec(window.location.pathname);
+    if (ma) {
+        clientId = ma[1];
+        document.getElementById("saddr").value = ma[1];
+        document.getElementById("raddr").value = ma[2];
+        document.getElementById("amt").value = ma[3];
+    }
 });
 
+// Arrange a graceful shutdown of the Web Sockets connection when
+// the brower window closes. It's not clear this makes any difference
+// or works across all browsers.
 window.addEventListener('beforeunload', function(event) {
-    ioSend(`LEAVE:${client_id}`);
+    webSocketSend(`LEAVE:${clientId}`);
 });
-
-window.addEventListener("load", onLoad);
-ioSend('play');
