d52d28b49e8c78ce2e29cb5d7cc83907a9f1c5ab
Keyao Shen <keyao@Keyaos-MacBook-Pro.local>
2021-08-25 12:04:04 -0700

    Add public key generation to multi-machine demo

--- examples/multi_machine/src/main.rs
+++ examples/multi_machine/src/main.rs
@@ -11,7 +11,7 @@ use phaselock::{
 use rand_xoshiro::{rand_core::SeedableRng, Xoshiro256StarStar};
 use serde::{de::DeserializeOwned, Serialize};
 use std::fs::File;
-use std::io::Read;
+use std::io::{prelude::*, Read};
 use std::path::Path;
 use structopt::StructOpt;
 use tagged_base64::TaggedBase64;
@@ -39,10 +39,35 @@ struct NodeOpt {
     )]
     config: String,
 
+    /// Whether to generate and store public keys for all nodes.
+    ///
+    /// Public keys will be stored under `examples/multi_machine/src`, file names starting
+    /// with `pk_`.
+    ///
+    /// Skip this option if public key files already exist.
+    #[structopt(long = "generate_keys", short = "g")]
+    generate_keys: bool,
+
     /// Id of the current node.
+    ///
     /// If the node ID is 0, it will propose and try to add transactions.
+    ///
+    /// Skip this option if only want to generate public key files.
     #[structopt(long = "id", short = "i")]
-    id: u64,
+    id: Option<u64>,
+}
+
+/// Gets public key of a node from its public key file.
+fn get_public_key(node_id: u64) -> PubKey {
+    let path_str = format!("../../examples/multi_machine/src/pk_{}", node_id);
+    let path = Path::new(&path_str);
+    let mut pk_file = File::open(&path)
+        .unwrap_or_else(|_| panic!("Cannot find public key file: {}", path.display()));
+    let mut pk_str = String::new();
+    pk_file
+        .read_to_string(&mut pk_str)
+        .unwrap_or_else(|err| panic!("Error while reading public key file: {}", err));
+    serde_json::from_str(&pk_str).expect("Error while reading public key")
 }
 
 /// Gets IP address and port number of a node from node configuration file.
@@ -59,11 +84,10 @@ fn get_host(node_config: Value, node_id: u64) -> (String, u16) {
 async fn get_networking<
     T: Clone + Serialize + DeserializeOwned + Send + Sync + std::fmt::Debug + 'static,
 >(
-    sks: &tc::SecretKeySet,
     node_id: u64,
     port: u16,
 ) -> (WNetwork<T>, PubKey) {
-    let pub_key = PubKey::from_secret_key_set_escape_hatch(sks, node_id);
+    let pub_key = get_public_key(node_id);
     debug!(?pub_key);
     let network = WNetwork::new(pub_key.clone(), port, None).await;
     if let Ok(n) = network {
@@ -86,7 +110,8 @@ async fn get_networking<
 
 /// Creates the initial state and phaselock for simulation.
 async fn init_state_and_phaselock(
-    sks: &tc::SecretKeySet,
+    public_keys: tc::PublicKeySet,
+    secret_key_share: tc::SecretKeyShare,
     nodes: u64,
     threshold: u64,
     node_id: u64,
@@ -120,9 +145,7 @@ async fn init_state_and_phaselock(
     .unwrap();
 
     // Create the initial phaselock
-    let known_nodes: Vec<_> = (0..nodes)
-        .map(|x| PubKey::from_secret_key_set_escape_hatch(sks, x))
-        .collect();
+    let known_nodes: Vec<_> = (0..nodes).map(|x| get_public_key(x)).collect();
 
     let config = PhaseLockConfig {
         total_nodes: nodes as u32,
@@ -137,8 +160,8 @@ async fn init_state_and_phaselock(
     let genesis = ElaboratedBlock::default();
     let (_, phaselock) = PhaseLock::init(
         genesis,
-        sks.public_keys(),
-        sks.secret_key_share(node_id),
+        public_keys,
+        secret_key_share,
         node_id,
         config,
         state.validator.clone(),
@@ -159,8 +182,6 @@ async fn main() {
     // Read configuration file path and node id from options
     let config_path_str = NodeOpt::from_args().config;
     let path = Path::new(&config_path_str);
-    let own_id = NodeOpt::from_args().id;
-    println!("Spawning network for node {}", own_id);
 
     // Read node info from node configuration file
     let mut config_file = File::open(&path)
@@ -168,12 +189,9 @@ async fn main() {
     let mut config_str = String::new();
     config_file
         .read_to_string(&mut config_str)
-        .unwrap_or_else(|err| panic!("Error while reading node config: [{}]", err));
-    let node_config: Value =
-        toml::from_str(&config_str).expect("Error while reading node config file");
-
-    // Get secret key set
-    let seed: u64 = node_config["seed"]
+        .unwrap_or_else(|err| panic!("Error while reading node config file: {}", err));
+    let node_config: Value = toml::from_str(&config_str).expect("Error while reading node config");
+    let seed = node_config["seed"]
         .as_integer()
         .expect("Missing seed value") as u64;
     let nodes = node_config["nodes"]
@@ -181,114 +199,151 @@ async fn main() {
         .expect("Missing nodes info")
         .len() as u64;
     let threshold = ((nodes * 2) / 3) + 1;
+
+    // Generate key sets
     let mut rng = Xoshiro256StarStar::seed_from_u64(seed);
-    let sks = tc::SecretKeySet::random(threshold as usize - 1, &mut rng);
+    let secret_keys = tc::SecretKeySet::random(threshold as usize - 1, &mut rng);
+    let public_keys = secret_keys.public_keys();
 
-    // Get networking information
-    let (own_network, _) =
-        get_networking(&sks, own_id, get_host(node_config.clone(), own_id).1).await;
-    #[allow(clippy::type_complexity)]
-    let mut other_nodes: Vec<(u64, PubKey, String, u16)> = Vec::new();
-    for id in 0..nodes {
-        if id != own_id {
-            let (ip, port) = get_host(node_config.clone(), id);
-            let pub_key = PubKey::from_secret_key_set_escape_hatch(&sks, id);
-            other_nodes.push((id, pub_key, ip, port));
+    // Generate public key for each node
+    if NodeOpt::from_args().generate_keys {
+        for node_id in 0..nodes {
+            let pub_key = PubKey::from_secret_key_set_escape_hatch(&secret_keys, node_id);
+            let pub_key_str = serde_json::to_string(&pub_key)
+                .unwrap_or_else(|err| panic!("Error while serializing the public key: {}", err));
+            let mut pk_file =
+                File::create(format!("../../examples/multi_machine/src/pk_{}", node_id))
+                    .unwrap_or_else(|err| {
+                        panic!("Error while creating a public key file: {}", err)
+                    });
+            pk_file.write(pub_key_str.as_bytes()).unwrap_or_else(|err| {
+                panic!("Error while writing to the public key file: {}", err)
+            });
         }
+        println!("Public key files created");
     }
 
-    // Connect the networking implementations
-    for (id, key, ip, port) in other_nodes {
-        let socket = format!("{}:{}", ip, port);
-        while own_network.connect_to(key.clone(), &socket).await.is_err() {
-            debug!("  - Retrying");
-            async_std::task::sleep(std::time::Duration::from_millis(10_000)).await;
+    if let Some(own_id) = NodeOpt::from_args().id {
+        println!("Current node: {}", own_id);
+        let secret_key_share = secret_keys.secret_key_share(own_id);
+
+        // Get networking information
+        let (own_network, _) =
+            get_networking(own_id, get_host(node_config.clone(), own_id).1).await;
+        #[allow(clippy::type_complexity)]
+        let mut other_nodes: Vec<(u64, PubKey, String, u16)> = Vec::new();
+        for id in 0..nodes {
+            if id != own_id {
+                let (ip, port) = get_host(node_config.clone(), id);
+                let pub_key = get_public_key(id);
+                other_nodes.push((id, pub_key, ip, port));
+            }
         }
-        println!("  - Connected to node {}", id);
-    }
 
-    // Wait for the networking implementations to connect
-    while (own_network.connection_table_size().await as u64) < nodes - 1 {
-        async_std::task::sleep(std::time::Duration::from_millis(10)).await;
-    }
-    println!("All nodes connected to network");
-
-    // Initialize the state and phaselock
-    let (mut state, mut phaselock) =
-        init_state_and_phaselock(&sks, nodes, threshold, own_id, own_network).await;
-
-    // Start consensus for each transaction
-    for round in 0..TRANSACTION_COUNT {
-        println!("Starting round {}", round + 1);
-
-        // Generate a transaction if the node ID is 0
-        let mut txn = None;
-        if own_id == 0 {
-            println!("  - Proposing a transaction");
-            let mut transactions = state
-                .generate_transactions(
-                    round as usize,
-                    vec![(0, 0, 0, 0, -2)],
-                    TRANSACTION_COUNT as usize,
-                )
-                .unwrap();
-            txn = Some(transactions.remove(0));
-            phaselock
-                .submit_transaction(txn.clone().unwrap().2)
+        // Connect the networking implementations
+        for (id, pub_key, ip, port) in other_nodes {
+            let socket = format!("{}:{}", ip, port);
+            while own_network
+                .connect_to(pub_key.clone(), &socket)
                 .await
-                .unwrap();
+                .is_err()
+            {
+                debug!("  - Retrying");
+                async_std::task::sleep(std::time::Duration::from_millis(10_000)).await;
+            }
+            println!("  - Connected to node {}", id);
         }
 
-        // Start consensus
-        // Note: wait until the transaction is proposed before starting consensus. Otherwise,
-        // the node will never reaches decision.
-        // Issue: https://gitlab.com/translucence/systems/system/-/issues/15.
-        let mut line = String::new();
-        println!("Hit the enter key when ready to start the consensus...");
-        std::io::stdin().read_line(&mut line).unwrap();
-        phaselock.start().await;
-        println!("  - Starting consensus");
-        let mut event: Event<ElaboratedBlock, ValidatorState> = phaselock
-            .next_event()
-            .await
-            .expect("PhaseLock unexpectedly closed");
-        while !matches!(event.event, EventType::Decide { .. }) {
-            event = phaselock
+        // Wait for the networking implementations to connect
+        while (own_network.connection_table_size().await as u64) < nodes - 1 {
+            async_std::task::sleep(std::time::Duration::from_millis(10)).await;
+        }
+        println!("All nodes connected to network");
+
+        // Initialize the state and phaselock
+        let (mut state, mut phaselock) = init_state_and_phaselock(
+            public_keys,
+            secret_key_share,
+            nodes,
+            threshold,
+            own_id,
+            own_network,
+        )
+        .await;
+
+        // Start consensus for each transaction
+        for round in 0..TRANSACTION_COUNT {
+            println!("Starting round {}", round + 1);
+
+            // Generate a transaction if the node ID is 0
+            let mut txn = None;
+            if own_id == 0 {
+                println!("  - Proposing a transaction");
+                let mut transactions = state
+                    .generate_transactions(
+                        round as usize,
+                        vec![(0, 0, 0, 0, -2)],
+                        TRANSACTION_COUNT as usize,
+                    )
+                    .unwrap();
+                txn = Some(transactions.remove(0));
+                phaselock
+                    .submit_transaction(txn.clone().unwrap().2)
+                    .await
+                    .unwrap();
+            }
+
+            // Start consensus
+            // Note: wait until the transaction is proposed before starting consensus. Otherwise,
+            // the node will never reaches decision.
+            // Issue: https://gitlab.com/translucence/systems/system/-/issues/15.
+            let mut line = String::new();
+            println!("Hit the return key when ready to start the consensus...");
+            std::io::stdin().read_line(&mut line).unwrap();
+            phaselock.start().await;
+            println!("  - Starting consensus");
+            let mut event: Event<ElaboratedBlock, ValidatorState> = phaselock
                 .next_event()
                 .await
                 .expect("PhaseLock unexpectedly closed");
-        }
-        if let EventType::Decide { block: _, state } = event.event {
-            let commitment = TaggedBase64::new("LEDG", &state.commit())
-                .unwrap()
-                .to_string();
-            println!("  - Current commitment: {}", commitment);
-        } else {
-            unreachable!();
+            while !matches!(event.event, EventType::Decide { .. }) {
+                event = phaselock
+                    .next_event()
+                    .await
+                    .expect("PhaseLock unexpectedly closed");
+            }
+            if let EventType::Decide { block: _, state } = event.event {
+                let commitment = TaggedBase64::new("LEDG", &state.commit())
+                    .unwrap()
+                    .to_string();
+                println!("  - Current commitment: {}", commitment);
+            } else {
+                unreachable!();
+            }
+
+            // Add the transaction if the node ID is 0
+            if let Some((ix, (owner_memos, k1_ix, k2_ix), t)) = txn {
+                println!("  - Adding the transaction");
+                let mut blk = ElaboratedBlock::default();
+                state
+                    .try_add_transaction(
+                        &mut blk,
+                        t,
+                        round as usize,
+                        ix,
+                        TRANSACTION_COUNT as usize,
+                        owner_memos,
+                        vec![k1_ix, k1_ix, k2_ix],
+                    )
+                    .unwrap();
+                state
+                    .validate_and_apply(blk, round as usize, TRANSACTION_COUNT as usize, 0.0)
+                    .unwrap();
+            }
+
+            println!("  - Round {} completed.", round + 1);
         }
 
-        // Add the transaction if the node ID is 0
-        if let Some((ix, (owner_memos, k1_ix, k2_ix), t)) = txn {
-            println!("  - Adding the transaction");
-            let mut blk = ElaboratedBlock::default();
-            state
-                .try_add_transaction(
-                    &mut blk,
-                    t,
-                    round as usize,
-                    ix,
-                    TRANSACTION_COUNT as usize,
-                    owner_memos,
-                    vec![k1_ix, k1_ix, k2_ix],
-                )
-                .unwrap();
-            state
-                .validate_and_apply(blk, round as usize, TRANSACTION_COUNT as usize, 0.0)
-                .unwrap();
-        }
-
-        println!("  - Round {} completed.", round + 1);
-    }
-
-    println!("All rounds completed.");
+        println!("All rounds completed.");
+    };
 }
