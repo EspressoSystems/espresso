f447e8375a62445bc16272f7fab50cc9b56430a8
Alex Xiong <alex.xiong.tech@gmail.com>
2022-03-02 18:30:28 +0800

    remove universal-param dep

--- zerok/zerok_lib/src/universal_params.rs
+++ zerok/zerok_lib/src/universal_params.rs
@@ -1,13 +1,21 @@
 #![deny(warnings)]
 use crate::state::MERKLE_HEIGHT;
+use jf_cap::structs::NoteType;
+use jf_cap::utils::compute_universal_param_size;
 use lazy_static::lazy_static;
 use rand_chacha::rand_core::SeedableRng;
 use rand_chacha::ChaChaRng;
 
-pub use universal_param::get as get_universal_param;
-pub use universal_param::set as set_universal_param;
-
 lazy_static! {
-    pub static ref UNIVERSAL_PARAM: jf_cap::proof::UniversalParam =
-        get_universal_param(&mut ChaChaRng::from_seed([0x8au8; 32]), MERKLE_HEIGHT);
+    pub static ref UNIVERSAL_PARAM: UniversalParam = {
+        let max_degree = compute_universal_param_size(NoteType::Transfer, 3, 3, MERKLE_HEIGHT)
+            .unwrap_or_else(|err| {
+                panic!(
+                    "Error while computing the universal parameter size for Transfer: {}",
+                    err
+                )
+            });
+        jf_cap::proof::universal_setup(max_degree, &mut ChaChaRng::from_seed([0u8; 32]))
+            .unwrap_or_else(|err| panic!("Error while generating universal param: {}", err))
+    };
 }
