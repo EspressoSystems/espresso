9956d9ad517c1363769dbec18fb4c8d11dd3bc4a
Nathan F Yospe <nyospe@gmail.com>
2022-04-20 15:02:59 -0400

    Inserting best-guess fields for validator status API

--- zerok/validator_status_api/src/query_data.rs
+++ zerok/validator_status_api/src/query_data.rs
@@ -1,3 +1,36 @@
-pub struct ValidatorStatus {
+use core::time::Duration;
+use phaselock::{
+    data::{
+        QuorumCertificate,
+    },
+    H_256,
+    PubKey,
+};
+use std::collections::HashMap;
+use zerok_lib::state::{
+    ElaboratedBlock,
+    ValidatorState,
+};
 
+pub struct PeerInfo {
+    pub peer_id: PubKey,
+}
+
+pub struct MempoolInfo {
+    pub transaction_count: u64,
+    pub output_count: u64,
+    pub memory_footprint: u64,
+}
+
+pub struct ValidatorStatus {
+    pub peer_list: Vec<PeerInfo>,
+    // TBD; these are going to correspond to active views, possibly want to also retain recent views?
+    // We may or may not want a seperate map of QuorumCertificates for `qc/:index` endpoint.
+    pub pending_blocks: HashMap<u64, (ElaboratedBlock, ValidatorState, QuorumCertificate<H_256>)>,
+    pub latest_block_id: u64, // id of latest block to reach DECIDE
+    pub mempool_info: MempoolInfo,
+    pub proposed_block_count: u64,
+    pub decided_block_count: u64,
+    pub abandoned_block_count: u64,
+    pub time_operational: Duration,
 }
