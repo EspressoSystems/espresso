8a81d42479964351afed739c18a2801b16b05232
Nathan F Yospe <nyospe@gmail.com>
2022-04-21 12:48:19 -0400

    Adding indexes to query data structs

--- zerok/archival_query_api/src/query_data.rs
+++ zerok/archival_query_api/src/query_data.rs
@@ -6,10 +6,13 @@ use zerok_lib::state::{
 pub struct BlockQueryData {
     pub raw_block: ElaboratedBlock,
     pub block_hash: BlockCommitment,
+    pub block_id: u64,
     pub txn_hashes: Vec<ElaboratedTransactionHash>,
 }
 
 pub struct StateQueryData {
     pub state: ValidatorState,
     pub commitment: LedgerStateCommitment,
+    pub block_id: u64,
+    pub event_index: u64,
 }
