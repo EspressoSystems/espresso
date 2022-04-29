b3af34f235153dee2b6733722eacaab5f3521982
Justin Restivo <justin@restivo.me>
2022-04-29 12:39:48 -0700

    feat: code coverage for espresso

--- flake.nix
+++ flake.nix
@@ -10,29 +10,16 @@
 
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , flake-compat
-    , rust-overlay
-    , ...
-    }:
+  outputs = { self, nixpkgs, flake-utils, flake-compat, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-    let
-      overlays = [ (import rust-overlay) ];
-      pkgs = import nixpkgs {
-        inherit system overlays;
-      };
-    in
-    {
-      devShell =
-        let
-
-          stableToolchain = pkgs.rust-bin.stable."1.59.0".minimal.override {
-            extensions = [ "rustfmt" "clippy" "llvm-tools-preview" "rust-src" ];
-          };
-          rustDeps = with pkgs; [
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        stableToolchain = pkgs.rust-bin.stable."1.59.0".minimal.override {
+          extensions = [ "rustfmt" "clippy" "llvm-tools-preview" "rust-src" ];
+        };
+        rustDeps = with pkgs;
+          [
             pkgconfig
             openssl
 
@@ -52,27 +39,54 @@
           ] ++ lib.optionals (stdenv.system != "aarch64-darwin") [
             cargo-watch # broken: https://github.com/NixOS/nixpkgs/issues/146349
           ];
-          # nixWithFlakes allows pre v2.4 nix installations to use
-          # flake commands (like `nix flake update`)
-          nixWithFlakes = pkgs.writeShellScriptBin "nix" ''
-            exec ${pkgs.nixFlakes}/bin/nix --experimental-features "nix-command flakes" "$@"
-          '';
-        in
-        pkgs.mkShell
-          {
-            buildInputs = with pkgs; [
+        # nixWithFlakes allows pre v2.4 nix installations to use
+        # flake commands (like `nix flake update`)
+        nixWithFlakes = pkgs.writeShellScriptBin "nix" ''
+          exec ${pkgs.nixFlakes}/bin/nix --experimental-features "nix-command flakes" "$@"
+        '';
+        cargo-llvm-cov = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "cargo-llvm-cov";
+          version = "0.3.0";
+
+          doCheck = false;
+
+          buildInputs = [ pkgs.libllvm ];
+
+          src = builtins.fetchTarball {
+            url =
+              "https://crates.io/api/v1/crates/${pname}/${version}/download";
+            sha256 =
+              "sha256:0iswa2cdaf2123vfc42yj9l8jx53k5jm2y51d4xqc1672hi4620l";
+          };
+
+          cargoSha256 = "sha256-RzIkW/eytU8ZdZ18x0sGriJ2xvjVW+8hB85In12dXMg=";
+          meta = with pkgs.lib; {
+            description = "Cargo llvm cov generates code coverage via llvm.";
+            homepage = "https://github.com/taiki-e/cargo-llvm-cov";
+
+            license = with licenses; [ mit asl20 ];
+          };
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs;
+            [
               nixWithFlakes
               nixpkgs-fmt
               git
               mdbook # make-doc, documentation generation
-            ]
-            ++ rustDeps;
+            ] ++ rustDeps;
 
-            RUST_SRC_PATH = "${stableToolchain}/lib/rustlib/src/rust/library";
-            RUST_BACKTRACE = 1;
-            RUST_LOG = "info";
+          RUST_SRC_PATH = "${stableToolchain}/lib/rustlib/src/rust/library";
+          RUST_BACKTRACE = 1;
+          RUST_LOG = "info";
+        };
+        devShells = {
+          perfShell = pkgs.mkShell {
+            buildInputs = with pkgs;
+              [ flamegraph fd cargo-llvm-cov stableToolchain ] ++ buildDeps;
           };
+        };
 
-    }
-    );
+      });
 }
