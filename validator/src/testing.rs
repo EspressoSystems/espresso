a6b2cf889edb10502546da0fedb41bc85954a352
Brendon Fish <bfish713@gmail.com>
2022-05-25 06:48:08 -0400

    Fix build issue and revert changes in validator

--- validator/src/testing.rs
+++ validator/src/testing.rs
@@ -73,8 +73,8 @@ pub struct TestNetwork {
     pub submit_api: Url,
     pub address_book_api: Url,
     pub nodes: Vec<TestNode>,
-    pub address_book: Option<AddressBook>,
-    pub _store: TempDir,
+    address_book: Option<AddressBook>,
+    _store: TempDir,
 }
 
 impl TestNetwork {
