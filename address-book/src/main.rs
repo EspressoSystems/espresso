6a559f4aa1e0667618533493165a43bb88d33050
Victor Koenders <gpg@trangar.com>
2022-05-31 12:03:58 +0200

    Fixed formatting and linting issues

--- address-book/src/main.rs
+++ address-book/src/main.rs
@@ -4,16 +4,13 @@
 // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+use address_book::{address_book_port, address_book_store_path, init_web_server, FileStore};
 use std::fs;
-use std::future::Future;
-use address_book::{
-    address_book_port, address_book_store_path, init_web_server, FileStore,
-};
 
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
     let cleanup_signals = register_interrupt_signals();
-    
+
     tracing_subscriber::fmt()
         .compact()
         .with_ansi(false)
@@ -38,25 +35,20 @@ async fn main() -> Result<(), std::io::Error> {
 }
 
 #[cfg(windows)]
-fn register_interrupt_signals() -> impl Future<Output = ()> {
-    // Signals aren't properly supported on windows so we'll just loop
-    async move {
-        loop {
-            async_std::task::sleep(std::time::Duration::from_secs(10)).await;
-        }
-    }
+async fn register_interrupt_signals() {
+    // Signals aren't properly supported on windows so we'll just exit
 }
 
 #[cfg(not(windows))]
-fn register_interrupt_signals() -> impl Future<Output = ()> {
+fn register_interrupt_signals() -> impl std::future::Future<Output = ()> {
+    use address_book::signal::handle_signals;
     use signal_hook::consts::{SIGINT, SIGTERM};
     use signal_hook_async_std::Signals;
-    use address_book::signal::handle_signals;
 
     let signals = Signals::new(&[SIGINT, SIGTERM]).expect("Failed to create signals.");
     let handle = signals.handle();
     let signals_task = async_std::task::spawn(handle_signals(signals));
-    
+
     async move {
         handle.close();
         signals_task.await;
