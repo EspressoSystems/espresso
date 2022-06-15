a092a5bd574e75bc94512a36996a099f5c521623
Brendon Fish <bfish713@gmail.com>
2022-06-15 12:19:34 -0400

    Add Mint, Freeze, and Unfreeze to random wallet test

--- zerok/zerok_client/src/bin/random_wallet.rs
+++ zerok/zerok_client/src/bin/random_wallet.rs
@@ -20,12 +20,17 @@
 // `-w KEY_FILE.pub` and pass the key pair to `random_keystore` with `-k KEY_FILE`.
 
 use async_std::task::sleep;
-use jf_cap::keys::UserPubKey;
-use jf_cap::structs::{AssetCode, AssetPolicy};
+use jf_cap::keys::{UserPubKey, FreezerPubKey};
+use jf_cap::structs::{AssetCode, AssetPolicy, FreezeFlag};
 use rand::distributions::weighted::WeightedError;
 use rand::seq::SliceRandom;
 use rand_chacha::{rand_core::SeedableRng, ChaChaRng};
+use rand::{
+    distributions::{Distribution, Standard},
+    Rng,
+};
 use seahorse::{events::EventIndex, hd::KeyTree, loader::KeystoreLoader, KeySnafu, KeystoreError};
+use seahorse::txn_builder::RecordInfo;
 use snafu::ResultExt;
 use std::fs::File;
 use std::io::Read;
@@ -39,8 +44,57 @@ use zerok_lib::{
     ledger::EspressoLedger,
     universal_params::UNIVERSAL_PARAM,
 };
+use std::collections::HashSet;
+
+#[derive(Debug)]
+pub enum OperationType {
+    Transfer,
+    Freeze,
+    Unfreeze,
+    Mint,
+}
+
+impl Distribution<OperationType> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> OperationType {
+        match rng.gen_range(0..=12) {
+            0 => OperationType::Mint,
+            1 => OperationType::Freeze,
+            2 => OperationType::Unfreeze,
+            // Bias toward transfer
+            _ => OperationType::Transfer,
+        }
+    }
+}
+
+
 
 type Keystore = seahorse::Keystore<'static, NetworkBackend<'static>, EspressoLedger, ()>;
+
+/// Return records the freezer has access to freeze or unfreeze but does not own.
+/// Will only return records with freeze_flag the same as the frozen arg.
+pub async fn find_freezable_records<'a>(
+    freezer: &Keystore,
+    frozen: FreezeFlag,
+) -> Vec<RecordInfo> {
+    let pks: HashSet<UserPubKey> = freezer.pub_keys().await.into_iter().collect();
+    let freeze_keys: HashSet<FreezerPubKey> =
+        freezer.freezer_pub_keys().await.into_iter().collect();
+    let records = freezer.records().await;
+    records
+        .filter(|r| {
+            let ro = &r.ro;
+            // Ignore records we own
+            if pks.contains(&ro.pub_key) {
+                return false;
+            }
+            // Check we can freeeze
+            if !(freeze_keys.contains(ro.asset_def.policy_ref().freezer_pub_key())) {
+                return false;
+            }
+            ro.freeze_flag == frozen
+        })
+        .collect()
+}
 
 #[derive(StructOpt)]
 struct Args {
@@ -290,47 +344,137 @@ async fn main() {
         // Randomly choose an asset type for the transfer.
         let asset = asset_balances.choose(&mut rng).unwrap();
 
+        let operation: OperationType = rand::random();
         // All transfers are the same, small size. This should prevent fragmentation errors and
         // allow us to make as many transactions as possible with the assets we have.
-        let amount = 1;
+        let amount = 10;
         let fee = 1;
 
-        event!(
-            Level::INFO,
-            "transferring {} units of {} to user {}",
-            amount,
-            if *asset == AssetCode::native() {
-                String::from("the native asset")
-            } else if *asset == my_asset.code {
-                String::from("my asset")
-            } else {
-                asset.to_string()
-            },
-            recipient,
-        );
-        let txn = match keystore
-            .transfer(Some(&address), asset, &[(recipient.clone(), amount)], fee)
-            .await
-        {
-            Ok(txn) => txn,
-            Err(err) => {
-                event!(Level::ERROR, "Error generating transfer: {}", err);
-                continue;
-            }
-        };
-        match keystore.await_transaction(&txn).await {
-            Ok(status) => {
-                if !status.succeeded() {
-                    // Transfers are allowed to fail. It can happen, for instance, if we get starved
-                    // out until our transfer becomes too old for the validators. Thus we make this
-                    // a warning, not an error.
-                    event!(Level::WARN, "transfer failed!");
+        match operation {
+            OperationType::Transfer => {
+                event!(
+                    Level::INFO,
+                    "transferring {} units of {} to user {}",
+                    amount,
+                    if *asset == AssetCode::native() {
+                        String::from("the native asset")
+                    } else if *asset == my_asset.code {
+                        String::from("my asset")
+                    } else {
+                        asset.to_string()
+                    },
+                    recipient,
+                );
+                let txn = match keystore
+                    .transfer(Some(&address), asset, &[(recipient.clone(), amount)], fee)
+                    .await
+                {
+                    Ok(txn) => txn,
+                    Err(err) => {
+                        event!(Level::ERROR, "Error generating transfer: {}", err);
+                        continue;
+                    }
+                };
+                match keystore.await_transaction(&txn).await {
+                    Ok(status) => {
+                        if !status.succeeded() {
+                            // Transfers are allowed to fail. It can happen, for instance, if we get starved
+                            // out until our transfer becomes too old for the validators. Thus we make this
+                            // a warning, not an error.
+                            event!(Level::WARN, "transfer failed!");
+                        }
+                    }
+                    Err(err) => {
+                        event!(Level::ERROR, "error while waiting for transaction: {}", err);
+                    }
                 }
-            }
-            Err(err) => {
-                event!(Level::ERROR, "error while waiting for transaction: {}", err);
-            }
+            },
+            OperationType::Mint => {
+                let new_asset = keystore
+                .define_asset(
+                    "Random keystore asset".to_string(),
+                    &[],
+                    AssetPolicy::default(),
+                )
+                .await
+                .expect("failed to define asset");
+
+                event!(Level::INFO, "defined a new asset type: {}", new_asset.code);
+
+                event!(Level::INFO, "minting my asset type {}", new_asset.code);
+                loop {
+                    let txn = keystore
+                        .mint(
+                            Some(&address),
+                            1,
+                            &new_asset.code,
+                            1u64 << 32,
+                            pub_key.clone(),
+                        )
+                        .await
+                        .expect("failed to generate mint transaction");
+                    let status = keystore
+                        .await_transaction(&txn)
+                        .await
+                        .expect("error waiting for mint to complete");
+                    if status.succeeded() {
+                        break;
+                    }
+                    // The mint transaction is allowed to fail due to contention from other clients.
+                    event!(Level::WARN, "mint transaction failed, retrying...");
+                    retry_delay().await;
+                }
+                event!(Level::INFO, "minted custom asset");
+            },
+            OperationType::Freeze => {
+                let freezable_records: Vec<RecordInfo> =
+                    find_freezable_records(&keystore, FreezeFlag::Unfrozen).await;
+                if freezable_records.is_empty() {
+                    event!(Level::INFO, "No freezable records");
+                    continue;
+                }
+                let record = freezable_records.choose(&mut rng).unwrap();
+                let owner_address = record.ro.pub_key.address().clone();
+                let asset_def = &record.ro.asset_def;
+                event!(
+                    Level::INFO,
+                    "Freezing Asset: {}, Amount: {}, Owner: {}",
+                    asset_def.code,
+                    record.ro.amount,
+                    owner_address
+                );
+                let freeze_address = keystore.pub_keys().await[0].address();
+
+                keystore.freeze(Some(&freeze_address), fee, &asset_def.code, record.amount(), owner_address)
+                    .await
+                    .ok();
+            },
+            OperationType::Unfreeze => {
+                event!(Level::INFO, "Unfreezing");
+                let freezable_records: Vec<RecordInfo> =
+                    find_freezable_records(&keystore, FreezeFlag::Frozen).await;
+                if freezable_records.is_empty() {
+                    event!(Level::INFO, "No frozen records");
+                    continue;
+                }
+                let record = freezable_records.choose(&mut rng).unwrap();
+                let owner_address = record.ro.pub_key.address();
+                let asset_def = &record.ro.asset_def;
+                event!(
+                    Level::INFO,
+                    "Unfreezing Asset: {}, Amount: {}, Owner: {}",
+                    asset_def.code,
+                    record.ro.amount,
+                    owner_address
+                );
+                let freeze_address = keystore.pub_keys().await[0].address();
+                keystore.unfreeze(Some(&freeze_address), fee, &asset_def.code, record.amount(), owner_address)
+                    .await
+                    .ok();
+            },
         }
+
+        
         event!(
             Level::INFO,
             "Wallet Native balance {}",
