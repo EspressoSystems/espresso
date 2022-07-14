fb958c8cf39e642c02a8eee9b49b74fcaf49c7d4
Jeb Bearer <jeb.bearer@translucence.net>
2022-07-14 13:00:34 -0700

    Fix slow tests build

--- faucet/src/faucet.rs
+++ faucet/src/faucet.rs
@@ -857,7 +857,7 @@ mod test {
     use async_std::task::{sleep, spawn_blocking};
     use escargot::CargoBuild;
     use espresso_validator::testing::minimal_test_network;
-    use futures::Future;
+    use futures::{Future, future::join_all};
     use jf_cap::structs::AssetDefinition;
     use net::client::response_body;
     use portpicker::pick_unused_port;
