6c28450213c29b9ffcdf8d5317f6d7fbb5dace7e
Jeb Bearer <jeb@espressosys.com>
2022-08-19 14:58:16 -0700

    Implement (non-subscription) status API

--- apis/catchup/src/api.rs
+++ apis/catchup/src/api.rs
@@ -10,13 +10,12 @@
 // You should have received a copy of the GNU General Public License along with this program. If not,
 // see <https://www.gnu.org/licenses/>.
 
-use crate::data_source::MetaStateDataSource;
+use crate::data_source::CatchUpDataSource;
 use clap::Args;
 use derive_more::From;
-use espresso_core::state::SetMerkleProof;
 use futures::FutureExt;
 use serde::{Deserialize, Serialize};
-use snafu::{OptionExt, Snafu};
+use snafu::Snafu;
 use std::path::PathBuf;
 use tide_disco::{
     api::{Api, ApiError},
@@ -26,35 +25,27 @@ use tide_disco::{
 
 #[derive(Args)]
 pub struct Options {
-    #[clap(long = "metastate-api-path", env = "ESPRESSO_METASTATE_API_PATH")]
+    #[clap(long = "catchup-api-path", env = "ESPRESSO_CATCHUP_API_PATH")]
     pub api_path: Option<PathBuf>,
 }
 
 #[derive(Clone, Debug, From, Snafu, Deserialize, Serialize)]
 pub enum Error {
     Request { source: RequestError },
-    InvalidBlockId { block_id: u64 },
 }
 
 impl Error {
     pub fn status(&self) -> StatusCode {
         match self {
             Self::Request { .. } => StatusCode::BadRequest,
-            Self::InvalidBlockId { .. } => StatusCode::BadRequest,
         }
     }
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct NullifierCheck {
-    spent: bool,
-    proof: SetMerkleProof,
 }
 
 pub fn define_api<State>(options: &Options) -> Result<Api<State, Error>, ApiError>
 where
     State: 'static + Send + Sync + ReadState,
-    <State as ReadState>::State: Sync + MetaStateDataSource,
+    for<'a> &'a <State as ReadState>::State: Send + Sync + CatchUpDataSource,
 {
     let mut api = match &options.api_path {
         Some(path) => Api::<State, Error>::from_file(path)?,
@@ -68,14 +59,18 @@ where
         }
     };
     api.with_version(env!("CARGO_PKG_VERSION").parse().unwrap())
-        .get("check_nullifier", |req, state| {
+        .get("get_events_since", |req, state| {
             async move {
-                let block_id = req.integer_param("block_id")?;
-                let nullifier = req.blob_param("nullifier")?;
-                let (spent, proof) = state
-                    .get_nullifier_proof_for(block_id, nullifier)
-                    .context(InvalidBlockIdSnafu { block_id })?;
-                Ok(NullifierCheck { spent, proof })
+                let first = req.integer_param("first")?;
+                if first >= state.len() {
+                    return Ok(vec![]);
+                }
+                let iter = state.get_nth_event_iter(first);
+                let mut events = iter.as_ref();
+                if let Some(count) = req.opt_integer_param("count")? {
+                    events = &events[..count];
+                }
+                Ok(events.to_vec())
             }
             .boxed()
         })?;
