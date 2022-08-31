e6bbf1c6f9856b7d72411d1e621809f5e7eb289f
Fernando Krell <fkrell@gmail.com>
2022-08-31 12:40:49 -0400

    a) Define RewardError, b) get KVMerkleProof from state

--- core/src/reward.rs
+++ core/src/reward.rs
@@ -13,10 +13,11 @@
 use crate::kv_merkle_tree::KVMerkleProof;
 use crate::stake_table::{
     StakeTableCommitment, StakeTableCommitmentsHash, StakeTableHash, StakingKey,
-    StakingKeySignature, ViewNumber,
+    StakingKeySignature, StakingPrivKey, ViewNumber,
 };
-use crate::state::{CommitableHash, CommitableHashTag};
+use crate::state::{CommitableHash, CommitableHashTag, ValidatorState};
 use ark_serialize::*;
+use ark_std::rand::{CryptoRng, RngCore};
 use core::hash::Hash;
 use jf_cap::keys::{UserAddress, UserPubKey};
 use jf_cap::structs::{
@@ -24,6 +25,7 @@ use jf_cap::structs::{
 };
 use jf_utils::tagged_blob;
 use serde::{Deserialize, Serialize};
+use snafu::Snafu;
 
 /// Previously collected rewards are recorded in (StakingKey, view_number) pairs
 #[tagged_blob("COLLECTED-REWARD")]
@@ -59,6 +61,44 @@ pub struct CollectRewardNote {
     signature: StakingKeySignature,
 }
 
+impl CollectRewardNote {
+    pub fn generate<R: CryptoRng + RngCore>(
+        rng: &mut R,
+        validator_state: &ValidatorState,
+        view_number: ViewNumber,
+        staking_priv_key: &StakingPrivKey,
+        cap_address: UserAddress,
+    ) -> Result<(Self, RewardNoteProofs), RewardError> {
+        let staking_public_key = StakingKey::from_priv_key(staking_priv_key);
+        let (body, proofs) = CollectRewardBody::generate(
+            rng,
+            validator_state,
+            view_number,
+            staking_public_key,
+            cap_address,
+        )?;
+        let size = CanonicalSerialize::serialized_size(&body);
+        let mut bytes = Vec::with_capacity(size);
+        CanonicalSerialize::serialize(&body, &mut bytes).map_err(RewardError::from)?;
+        let note = CollectRewardNote {
+            body,
+            signature: StakingKey::sign(staking_priv_key, &bytes),
+        };
+
+        Ok((note, proofs))
+    }
+}
+
+impl CollectRewardNote {
+    pub(crate) fn output_commitment(&self) -> RecordCommitment {
+        self.body.output_commitment()
+    }
+
+    pub(crate) fn output_opening(&self) -> RecordOpening {
+        self.body.output_opening()
+    }
+}
+
 /// Reward Collection Transaction Note
 #[derive(
     Clone,
@@ -83,6 +123,33 @@ pub struct CollectRewardBody {
 }
 
 impl CollectRewardBody {
+    pub fn generate<R: RngCore + CryptoRng>(
+        rng: &mut R,
+        validator_state: &ValidatorState,
+        view_number: ViewNumber,
+        staking_key: StakingKey,
+        cap_address: UserAddress,
+    ) -> Result<(Self, RewardNoteProofs), RewardError> {
+        let blind_factor = BlindFactor::rand(rng);
+        let (rewards_proofs, stake_amount) =
+            RewardNoteProofs::generate(validator_state, view_number, &staking_key)?;
+        let vrf_witness = VrfWitness {
+            staking_key,
+            view_number,
+            stake_amount,
+        };
+        let reward_amount = Amount::from(0u128); // TODO how to compute reward amount?
+        let body = CollectRewardBody {
+            blind_factor,
+            cap_address,
+            reward_amount,
+            vrf_witness,
+        };
+        Ok((body, rewards_proofs))
+    }
+}
+
+impl CollectRewardBody {
     pub(crate) fn output_commitment(&self) -> RecordCommitment {
         RecordCommitment::from(&self.output_opening())
     }
@@ -95,16 +162,6 @@ impl CollectRewardBody {
             freeze_flag: FreezeFlag::Unfrozen,
             blind: self.blind_factor,
         }
-    }
-}
-
-impl CollectRewardNote {
-    pub(crate) fn output_commitment(&self) -> RecordCommitment {
-        self.body.output_commitment()
-    }
-
-    pub(crate) fn output_opening(&self) -> RecordOpening {
-        self.body.output_opening()
     }
 }
 
@@ -157,4 +214,102 @@ pub struct RewardNoteProofs {
     stake_amount_proof: KVMerkleProof<StakeTableHash>,
     /// Proof that reward hasn't been collected
     uncollected_reward_proof: KVMerkleProof<CollectedRewardsHash>,
+}
+
+impl RewardNoteProofs {
+    /// Return RewardNoteHelperProofs if view_number is valid and staking_key was elected for it, otherwise return RewardError.
+    pub(crate) fn generate(
+        validator_state: &ValidatorState,
+        view_number: ViewNumber,
+        stake_key: &StakingKey,
+    ) -> Result<(Self, Amount), RewardError> {
+        let (stake_table_commitment, stake_table_commitment_proof) =
+            Self::get_stake_commitment_and_proof(validator_state, view_number)?;
+        let (stake_amount, stake_amount_proof) =
+            Self::get_amount_and_proof(validator_state, stake_key)?;
+        let uncollected_reward_proof =
+            Self::proof_uncollected_rewards(validator_state, stake_key, view_number)?;
+        Ok((
+            Self {
+                stake_table_commitment,
+                stake_table_commitment_proof,
+                stake_amount_proof,
+                uncollected_reward_proof,
+            },
+            stake_amount,
+        ))
+    }
+
+    /// Returns StakeTable commitment for view number, if view number is valid, otherwise return RewardError::InvalidViewNumber
+    fn get_stake_commitment_and_proof(
+        validator_state: &ValidatorState,
+        view_number: ViewNumber,
+    ) -> Result<
+        (
+            StakeTableCommitment,
+            KVMerkleProof<StakeTableCommitmentsHash>,
+        ),
+        RewardError,
+    > {
+        let (option_value, proof) = validator_state
+            .stake_table_commitments
+            .lookup(view_number)
+            .ok_or(RewardError::ProofNotInMemory {})?;
+        let commitment = option_value.ok_or(RewardError::InvalidViewNumber { view_number })?;
+        Ok((commitment, proof))
+    }
+
+    /// Returns amount and proof is staking key is found on stake table, otherwise returns RewardError::StakingKeyNotFound
+    fn get_amount_and_proof(
+        _validator_state: &ValidatorState,
+        _staking_key: &StakingKey,
+    ) -> Result<(Amount, KVMerkleProof<StakeTableHash>), RewardError> {
+        unimplemented!()
+    }
+
+    /// Returns proof if reward hasn't been collected, otherwise RewardError::RewardAlreadyCollected
+    fn proof_uncollected_rewards(
+        validator_state: &ValidatorState,
+        staking_key: &StakingKey,
+        view_number: ViewNumber,
+    ) -> Result<KVMerkleProof<CollectedRewardsHash>, RewardError> {
+        let key = CollectedRewards((staking_key.clone(), view_number));
+        let (found, proof) = validator_state
+            .collected_rewards
+            .lookup(key)
+            .ok_or(RewardError::ProofNotInMemory {})?;
+        if found.is_none() {
+            Ok(proof)
+        } else {
+            Err(RewardError::RewardAlreadyCollected {})
+        }
+    }
+}
+
+/// Reward Transaction Errors.
+#[derive(Debug, Snafu, Serialize, Deserialize)]
+#[snafu(visibility(pub(crate)))]
+pub enum RewardError {
+    /// An invalid view number
+    InvalidViewNumber { view_number: ViewNumber },
+
+    /// Serialization error
+    SerializationError { reason: String },
+
+    /// StakingKeyNotFound
+    StakingKeyNotFound {},
+
+    /// Reward has been already collected
+    RewardAlreadyCollected {},
+
+    /// Proof not in memory
+    ProofNotInMemory {},
+}
+
+impl From<ark_serialize::SerializationError> for RewardError {
+    fn from(source: ark_serialize::SerializationError) -> Self {
+        Self::SerializationError {
+            reason: source.to_string(),
+        }
+    }
 }
