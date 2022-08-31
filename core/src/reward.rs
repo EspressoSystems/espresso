1d5a2140db094d0e56d642e166393a97acfcb1c2
Fernando Krell <fkrell@users.noreply.github.com>
2022-08-31 14:08:34 -0400

    Cleanup Stake Table structs (#589)
    * Cleanup Stake Table structs
    
    * Split RewardNote into Body and Signature
    
    * Implemented signatures for StakingKey

--- core/src/reward.rs
+++ core/src/reward.rs
@@ -12,17 +12,35 @@
 
 use crate::kv_merkle_tree::KVMerkleProof;
 use crate::stake_table::{
-    CollectedRewardsHash, StakeKeyToStakeAmountCommittableHash, StakeTableCommitmentsHash,
+    StakeTableCommitment, StakeTableCommitmentsHash, StakeTableHash, StakingKey,
+    StakingKeySignature, ViewNumber,
 };
-use crate::state::StakingKey;
-use crate::tree_hash::KVTreeHash;
+use crate::state::{CommitableHash, CommitableHashTag};
 use ark_serialize::*;
 use core::hash::Hash;
 use jf_cap::keys::{UserAddress, UserPubKey};
 use jf_cap::structs::{
     Amount, AssetDefinition, BlindFactor, FreezeFlag, RecordCommitment, RecordOpening,
 };
+use jf_utils::tagged_blob;
 use serde::{Deserialize, Serialize};
+
+/// Previously collected rewards are recorded in (StakingKey, view_number) pairs
+#[tagged_blob("COLLECTED-REWARD")]
+#[derive(Clone, Debug, PartialEq, Eq, Hash, CanonicalSerialize, CanonicalDeserialize)]
+pub struct CollectedRewards((StakingKey, ViewNumber));
+
+/// Identifying tag for CollectedReward
+#[derive(Clone, Debug, Copy, PartialEq, Eq, Serialize, Deserialize, Hash)]
+pub struct CollectedRewardsTag();
+impl CommitableHashTag for CollectedRewardsTag {
+    fn commitment_diversifier() -> &'static str {
+        "Collected rewards"
+    }
+}
+
+/// Hash for set Merkle tree for all of the previously-collected rewards
+pub type CollectedRewardsHash = CommitableHash<CollectedRewards, (), CollectedRewardsTag>;
 
 /// Reward Collection Transaction Note
 #[derive(
@@ -37,6 +55,23 @@ use serde::{Deserialize, Serialize};
     Deserialize,
 )]
 pub struct CollectRewardNote {
+    body: CollectRewardBody,
+    signature: StakingKeySignature,
+}
+
+/// Reward Collection Transaction Note
+#[derive(
+    Clone,
+    Debug,
+    PartialEq,
+    Eq,
+    Hash,
+    CanonicalSerialize,
+    CanonicalDeserialize,
+    Serialize,
+    Deserialize,
+)]
+pub struct CollectRewardBody {
     /// Blinding factor for reward record commitment on CAP native asset
     blind_factor: BlindFactor,
     /// Address that owns the reward
@@ -44,10 +79,10 @@ pub struct CollectRewardNote {
     /// Reward amount
     reward_amount: Amount,
     /// Staking `pub_key`, `view` number and a proof that staking key was selected for committee election on `view`
-    vrf_witness_info: VrfWitness,
+    vrf_witness: VrfWitness,
 }
 
-impl CollectRewardNote {
+impl CollectRewardBody {
     pub(crate) fn output_commitment(&self) -> RecordCommitment {
         RecordCommitment::from(&self.output_opening())
     }
@@ -60,6 +95,16 @@ impl CollectRewardNote {
             freeze_flag: FreezeFlag::Unfrozen,
             blind: self.blind_factor,
         }
+    }
+}
+
+impl CollectRewardNote {
+    pub(crate) fn output_commitment(&self) -> RecordCommitment {
+        self.body.output_commitment()
+    }
+
+    pub(crate) fn output_opening(&self) -> RecordOpening {
+        self.body.output_opening()
     }
 }
 
@@ -78,7 +123,7 @@ struct VrfWitness {
     /// Staking public key
     staking_key: StakingKey,
     /// View number for which the key was elected
-    view_number: u64,
+    view_number: ViewNumber,
     /// amount of stake on `view_number`
     stake_amount: Amount, /*
                           /// VRF Proof
@@ -105,11 +150,11 @@ struct VrfWitness {
 )]
 pub struct RewardNoteProofs {
     /// Stake table commitment for the view number reward
-    stake_table_commitment: <StakeTableCommitmentsHash as KVTreeHash>::Digest,
+    stake_table_commitment: StakeTableCommitment,
     /// Proof for stake_table_commitment
-    view_number_to_stake_table_commitment: KVMerkleProof<StakeTableCommitmentsHash>,
+    stake_table_commitment_proof: KVMerkleProof<StakeTableCommitmentsHash>,
     /// Proof for stake_amount for staking key on that view number
-    staking_key_to_stake_amount: KVMerkleProof<StakeKeyToStakeAmountCommittableHash>,
+    stake_amount_proof: KVMerkleProof<StakeTableHash>,
     /// Proof that reward hasn't been collected
     uncollected_reward_proof: KVMerkleProof<CollectedRewardsHash>,
 }
