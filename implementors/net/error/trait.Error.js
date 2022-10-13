9800a30f4bea4a0a5461ca5026d60f48c47448b0
shenkeyao <shenkeyao@users.noreply.github.com>
2022-10-13 16:38:24 +0000

    deploy: d56fd693d285df4e6d6fe93bfb7656e1e2235525

--- implementors/net/error/trait.Error.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["faucet_types"] = [{"text":"impl Error for <a class=\"enum\" href=\"faucet_types/enum.FaucetError.html\" title=\"enum faucet_types::FaucetError\">FaucetError</a>","synthetic":false,"types":["faucet_types::FaucetError"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
