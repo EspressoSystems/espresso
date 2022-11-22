7631c03e9774fb2f4eddb041135b963cbea18b5b
Jeb Bearer <jeb@espressosys.com>
2022-11-21 16:01:05 -0800

    Pin dependencies and delete unused code

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -11,7 +11,6 @@
 // You should have received a copy of the GNU General Public License along with this program. If not,
 // see <https://www.gnu.org/licenses/>.
 
-pub mod full_persistence;
 pub mod genesis;
 pub mod kv_merkle_tree;
 pub mod ledger;
